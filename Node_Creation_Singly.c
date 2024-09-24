fed49efeeb68066b8460dbe1a93260a95b15be49
Hrishikesh Divekar <maduchiha161@gmail.com>
2024-09-24 11:22:04 +0530

    Singly Linked Node

--- Node_Creation_Singly.c
+++ Node_Creation_Singly.c
@@ -1,1 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node *next;
+}*head;
+
+createNode(int data){
+  struct node *newnode;
+  newnode = (struct node)malloc(sizeof(struct node));
+  
+}
